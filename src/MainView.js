9d3cf502d29b98004d46d6a0f7393899e8d6645c
Kenechukwu <Allisonkosy@gmail.com>
2021-05-18 07:29:15 +0100

    Update MainView.js

--- src/MainView.js
+++ src/MainView.js
@@ -126,23 +126,25 @@ const USE_VIDEO = true;
 
 
 const servers ={
-iceServers: [
-{
-
-urls: [
-
-"stun:stun1.l.google.com:19302","stun:stun2.l.google.com:19302"
-
-]
-},
-	
-{ urls: "stun:stun.stunprotocol.org:3478" },
+iceServers: 
+[
+	{ urls: "stun:stun.l.google.com:19302" },
+	{ urls: "stun:stun.stunprotocol.org:3478" },
 	{ urls: "stun:stun.sipnet.net:3478" },
 	{ urls: "stun:stun.ideasip.com:3478" },
 	{ urls: "stun:stun.iptel.org:3478" },
-],
-
-iceCandidatePoolSize:10
+	{ urls: "turn:numb.viagenie.ca", username: "imvasanthv@gmail.com", credential: "d0ntuseme" },
+	{
+		urls: [
+			"turn:173.194.72.127:19305?transport=udp",
+			"turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
+			"turn:173.194.72.127:443?transport=tcp",
+			"turn:[2404:6800:4008:C01::7F]:443?transport=tcp",
+		],
+		username: "CKjCuLwFEgahxNRjuTAYzc/s6OMT",
+		credential: "u1SQDR/SQsPQIxXNWQT7czc/G4c=",
+	},
+]
 
 
 
